e3952bc9be331def602cd1947fd5d599ea21b1a0
Ian-Wanjira <ianwanjira4@gmail.com>
2024-08-27 17:13:30 -0500

    feat: create CustomUserAdmin

--- patient/admin.py
+++ patient/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    # Define the fields to be displayed in the admin interface
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {"fields": ("name", "phoneNumber", "dateOfBirth", "address")},
+        ),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "name", "phoneNumber", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "name", "is_staff", "is_superuser")
+    search_fields = ("email", "name")
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
