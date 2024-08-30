e5da2e894884cea3d2777d68ddd3f9e59ccc9d18
Ian-Wanjira <ianwanjira4@gmail.com>
2024-08-30 05:03:01 -0500

    fix: add id field in admin view

--- patient/admin.py
+++ patient/admin.py
@@ -14,6 +14,7 @@ class CustomUserAdmin(UserAdmin):
         ),
         ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
+        ("Identification", {"fields": ("id",)}),
     )
     add_fieldsets = (
         (
@@ -24,17 +25,17 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
     )
-    list_display = ("email", "name", "is_staff", "is_superuser")
-    search_fields = ("email", "name")
+    list_display = ("id", "email", "name", "is_staff", "is_superuser")
+    search_fields = ("id", "email", "name")
     ordering = ("email",)
 
     # Prevent 'date_joined' from being editable
     def get_readonly_fields(self, request, obj=None):
         if obj:  # Editing an existing user
-            return self.readonly_fields + ("date_joined",)
+            return self.readonly_fields + ("date_joined", "id")
         return self.readonly_fields
 
-    readonly_fields = ("date_joined",)
+    readonly_fields = ("date_joined", "id")
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
