8edc69d76482f3ac184eb1e8ddb5a9d2e9b314e6
Ian-Wanjira <ianwanjira4@gmail.com>
2024-08-30 08:02:45 -0500

    feat: add identificationDocument property to CompleteRegistrationSerializer

--- patient/serializers.py
+++ patient/serializers.py
@@ -11,20 +11,26 @@ class InitialRegistrationSerializer(serializers.ModelSerializer):
 
 class CompleteRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
+    identificationDocument = serializers.FileField(required=False)
 
     class Meta:
         model = CustomUser
         fields = "__all__"
         extra_kwargs = {
-            "password": {"write_only": True},  # Ensure password is write-only
+            "password": {"write_only": True},
+            "identificationDocument": {"required": False},  # Optional field
         }
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
+        identification_document = validated_data.pop("identificationDocument", None)
         user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
-            user.save()
 
+        if identification_document:
+            user.identificationDocument = identification_document
+
+        user.save()
         return user
