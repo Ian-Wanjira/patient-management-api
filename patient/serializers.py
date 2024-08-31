353ee3da984525feceb0256a1667d9cf4e005e57
Ian-Wanjira <ianwanjira4@gmail.com>
2024-08-31 06:28:10 -0500

    feat: print out details about the validated data

--- patient/serializers.py
+++ patient/serializers.py
@@ -11,26 +11,26 @@ class InitialRegistrationSerializer(serializers.ModelSerializer):
 
 class CompleteRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
-    identificationDocument = serializers.FileField(required=False)
 
     class Meta:
         model = CustomUser
         fields = "__all__"
         extra_kwargs = {
             "password": {"write_only": True},
-            "identificationDocument": {"required": False},  # Optional field
         }
 
     def update(self, instance, validated_data):
         password = validated_data.pop("password", None)
-        identification_document = validated_data.pop("identificationDocument", None)
-        user = super().update(instance, validated_data)
+
+        # Debug: Check what's inside validated_data
+        print("Validated Data:", validated_data)
+
+        # Update the instance with the validated data
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
 
         if password:
-            user.set_password(password)
+            instance.set_password(password)
 
-        if identification_document:
-            user.identificationDocument = identification_document
-
-        user.save()
-        return user
+        instance.save()
+        return instance
