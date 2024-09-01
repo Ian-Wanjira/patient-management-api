c67794ecd69e30be4a99bc68756ccf24abc374a1
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-01 16:18:19 -0500

    refactor: change views to use RegistrationView

--- patient/urls.py
+++ patient/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import CompleteRegistrationView, InitialRegistrationView, PatientProfileView
+from .views import RegistrationView
 
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(
-        "get-started/", InitialRegistrationView.as_view(), name="initial-registration"
-    ),
-    path(
         "complete-registration/",
-        CompleteRegistrationView.as_view(),
+        RegistrationView.as_view(),
         name="complete-registration",
     ),
-    path("<uuid:id>/", PatientProfileView.as_view(), name="patient-profile"),
 ]
