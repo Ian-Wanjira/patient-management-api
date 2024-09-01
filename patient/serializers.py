fc64df10ae49af9039d93e1c4d113ee2bc0cfd87
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-01 16:17:13 -0500

    refactor: change serializer classes to RegistrationSerializer

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,36 +1,14 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import CustomUser
 
 
-class InitialRegistrationSerializer(serializers.ModelSerializer):
+class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ["email", "name", "phone"]
-
-
-class CompleteRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=False)
-
-    class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = "__all__"
-        extra_kwargs = {
-            "password": {"write_only": True},
-        }
 
-    def update(self, instance, validated_data):
-        password = validated_data.pop("password", None)
-
-        # Debug: Check what's inside validated_data
-        print("Validated Data:", validated_data)
-
-        # Update the instance with the validated data
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        if password:
-            instance.set_password(password)
-
-        instance.save()
-        return instance
+    def create(self, validated_data):
+        patient = CustomUser.objects.create_user(**validated_data)
+        return patient
