20466e51dc48d1520d5c72dda604863e77d90c1f
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-01 16:13:53 -0500

    fix: clear admin file

--- patient/admin.py
+++ patient/admin.py
@@ -1,65 +1,66 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib import admin
 
-from .models import CustomUser
+# from django.contrib.auth.admin import UserAdmin
 
-
-class CustomUserAdmin(UserAdmin):
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (
-            "Personal info",
-            {"fields": ("name", "phone", "dateOfBirth", "address", "gender")},
-        ),
-        (
-            "Medical info",
-            {
-                "fields": (
-                    "emergencyContactName",
-                    "emergencyContactPhone",
-                    "insuranceProvider",
-                    "insurancePolicyNumber",
-                    "allergies",
-                    "currentMedications",
-                    "familyMedicalHistory",
-                    "pastMedicalHistory",
-                    "identificationType",
-                    "identificationNumber",
-                    "identificationDocument",
-                    "primaryCarePhysician",
-                )
-            },
-        ),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-        ("Identification", {"fields": ("id",)}),
-    )
-
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "name", "phone", "password1", "password2"),
-            },
-        ),
-    )
-
-    list_display = (
-        "id",
-        "email",
-        "name",
-        "phone",
-        "is_staff",
-        "is_superuser",
-        "gender",
-        "dateOfBirth",
-        "address",
-    )
-    search_fields = ("id", "email", "name", "phone")
-    ordering = ("email",)
-
-    readonly_fields = ("date_joined", "id")
+# from .models import CustomUser
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+# class CustomUserAdmin(UserAdmin):
+#     fieldsets = (
+#         (None, {"fields": ("email", "password")}),
+#         (
+#             "Personal info",
+#             {"fields": ("name", "phone", "dateOfBirth", "address", "gender")},
+#         ),
+#         (
+#             "Medical info",
+#             {
+#                 "fields": (
+#                     "emergencyContactName",
+#                     "emergencyContactPhone",
+#                     "insuranceProvider",
+#                     "insurancePolicyNumber",
+#                     "allergies",
+#                     "currentMedications",
+#                     "familyMedicalHistory",
+#                     "pastMedicalHistory",
+#                     "identificationType",
+#                     "identificationNumber",
+#                     "identificationDocument",
+#                     "primaryCarePhysician",
+#                 )
+#             },
+#         ),
+#         ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+#         ("Important dates", {"fields": ("last_login", "date_joined")}),
+#         ("Identification", {"fields": ("id",)}),
+#     )
+
+#     add_fieldsets = (
+#         (
+#             None,
+#             {
+#                 "classes": ("wide",),
+#                 "fields": ("email", "name", "phone", "password1", "password2"),
+#             },
+#         ),
+#     )
+
+#     list_display = (
+#         "id",
+#         "email",
+#         "name",
+#         "phone",
+#         "is_staff",
+#         "is_superuser",
+#         "gender",
+#         "dateOfBirth",
+#         "address",
+#     )
+#     search_fields = ("id", "email", "name", "phone")
+#     ordering = ("email",)
+
+#     readonly_fields = ("date_joined", "id")
+
+
+# admin.site.register(CustomUser, CustomUserAdmin)
