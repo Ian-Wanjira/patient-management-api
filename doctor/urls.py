897b3a3fbcd7162ca1c44170cf50e017c1cbbe19
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-21 07:45:26 -0500

    feat: add path to list all doctors

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RegistrationView
+from .views import DoctorListView, RegistrationView
 
 urlpatterns = [
     path("register/", RegistrationView.as_view(), name="register"),
+    path("list/", DoctorListView.as_view(), name="list"),
 ]
