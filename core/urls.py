ce0033572a6be214fc389cb92b69dfa16d7e8c0d
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-21 07:25:29 -0500

    feat: add doctor.urls to core urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/patient/", include("patient.urls")),
+    path("api/doctor/", include("doctor.urls")),
 ]
 
 if settings.DEBUG:
