81474075fff8cc66919b0a28c2b9d04e5c0ff5d3
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-21 07:26:37 -0500

    feat: create doctor model

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,13 @@
-from django.db import models  # noqa: F401
+import uuid
 
-# Create your models here.
+from django.db import models
+
+
+class Doctor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    image = models.ImageField(upload_to="doctor_images")
+
+    def __str__(self):
+        self.email
