03a98a888b2198f06fd6fc58bff2200f839df8b7
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-24 07:13:01 -0500

    refactor: change endpoint of doctor registration from register/ to doctor-registration/

--- doctor/urls.py
+++ doctor/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import DoctorListView, RegistrationView
 
 urlpatterns = [
-    path("register/", RegistrationView.as_view(), name="register"),
+    path("doctor-registration/", RegistrationView.as_view(), name="register"),
     path("list/", DoctorListView.as_view(), name="list"),
 ]
