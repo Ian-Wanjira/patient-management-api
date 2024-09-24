a1a134806011667f5675d59608b9fb927d937f85
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-24 03:26:54 -0500

    refactor: change and flatten patient profile creation

--- patient/serializers.py
+++ patient/serializers.py
@@ -1,18 +1,49 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import CustomUser
+from accounts.models import CustomUser
+
+from .models import Patient
 
 
-class RegistrationSerializer(serializers.ModelSerializer):
+class PatientSerializer(serializers.ModelSerializer):
+    # User fields flattened into the serializer
+    id = serializers.UUIDField(source="user.id", read_only=True)
+    email = serializers.EmailField(source="user.email")
+    name = serializers.CharField(source="user.name")
+    phone = serializers.CharField(source="user.phone")
+    password = serializers.CharField(write_only=True, source="user.password")
+
     class Meta:
-        model = get_user_model()
-        fields = "__all__"
-
-        extra_kwargs = {
-            "password": {"write_only": True},
-        }
+        model = Patient
+        fields = [
+            "id",
+            "email",
+            "name",
+            "phone",
+            "password",
+            "address",
+            "occupation",
+            "emergencyContactName",
+            "emergencyContactPhone",
+            "insuranceProvider",
+            "insurancePolicyNumber",
+            "allergies",
+            "currentMedications",
+            "familyMedicalHistory",
+            "pastMedicalHistory",
+            "identificationType",
+            "identificationNumber",
+            "identificationDocument",
+            "primaryCarePhysician",
+            "treatmentConsent",
+            "disclosureConsent",
+            "privacyConsent",
+            "dateOfBirth",
+        ]
 
     def create(self, validated_data):
-        patient = CustomUser.objects.create_user(**validated_data)
+        # Extract user data from the flattened fields
+        user_data = validated_data.pop("user")
+        user = CustomUser.objects.create_user(**user_data)
+        patient = Patient.objects.create(user=user, **validated_data)
         return patient
