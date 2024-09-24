b693da399e55267b4c0b89093c24355ffd16f4fe
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-24 03:24:53 -0500

    refactor: add patient models in patient app

--- patient/models.py
+++ patient/models.py
@@ -1,54 +1,17 @@
-import uuid
-
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-
-        try:
-            user = self.model(email=email, **extra_fields)
-            user.set_password(password)
-            user.save(using=self._db)
-            return user
-        except Exception as e:
-            print(e)
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True")
-
-        return self.create_user(email, password, **extra_fields)
+from accounts.models import CustomUser
 
 
-class CustomUser(AbstractBaseUser):
-    GENDER_CHOICES = [
-        ("male", "Male"),
-        ("female", "Female"),
-        ("other", "Other"),
-    ]
+class Patient(models.Model):
+    GENDER_CHOICES = [("male", "Male"), ("female", "Female"), ("other", "Other")]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = None  # Remove username field as we're using email for authentication
-    email = models.EmailField(unique=True)
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
+    user = models.OneToOneField(
+        CustomUser, on_delete=models.CASCADE, related_name="patient_profile"
+    )
 
-    # Optional fields to be filled out later
     gender = models.CharField(
-        max_length=10,
-        choices=GENDER_CHOICES,
-        blank=True,
-        null=True,
+        max_length=10, choices=GENDER_CHOICES, blank=True, null=True
     )
     address = models.CharField(max_length=100, blank=True, null=True)
     occupation = models.CharField(max_length=100, blank=True, null=True)
@@ -70,18 +33,3 @@ class CustomUser(AbstractBaseUser):
     disclosureConsent = models.BooleanField(default=False)
     privacyConsent = models.BooleanField(default=False)
     dateOfBirth = models.DateField(null=True, blank=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.email
-
-    def get_full_name(self):
-        return f"{self.name}"
