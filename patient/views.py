efefb48815c14a444b0f35bfa23cab08e5642e7c
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-24 03:29:27 -0500

    refactor: change from using RegistrationSerializer to PatientSerializer

--- patient/views.py
+++ patient/views.py
@@ -2,13 +2,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer
+from .serializers import PatientSerializer
 
 
-class RegistrationView(APIView):
+class RegisterPatientView(APIView):
     def post(self, request):
         print("Request Data: ", request.data)
-        serializer = RegistrationSerializer(data=request.data)
+        serializer = PatientSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
