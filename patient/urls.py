896bbec3ef98afd1fe9e58f45dc2c0e635225a63
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-24 03:27:51 -0500

    refactor: change how patient is registered by seperation of concerns

--- patient/urls.py
+++ patient/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegistrationView
+from .views import RegisterPatientView
 
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(
-        "complete-registration/",
-        RegistrationView.as_view(),
-        name="complete-registration",
+        "patient-registration/",
+        RegisterPatientView.as_view(),
+        name="register_patient",
     ),
 ]
