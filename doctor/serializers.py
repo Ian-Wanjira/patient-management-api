d226a0513c8b23c5c60690e1e7095b4252f53c2f
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-24 07:12:01 -0500

    refactor: change DoctorSerializer to flatten CustomUser fields and create a CustomUser whenever a Patient is created

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 
+from accounts.models import CustomUser
+
 from .models import Doctor
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(source="user.id", read_only=True)
+    email = serializers.EmailField(source="user.email")
+    name = serializers.CharField(source="user.name")
+    phone = serializers.CharField(source="user.phone")
+    password = serializers.CharField(write_only=True, source="user.password")
+
+    image = serializers.ImageField()
+
     class Meta:
         model = Doctor
-        fields = "__all__"
+        fields = ["id", "email", "name", "phone", "password", "image"]
 
     def create(self, validated_data):
-        doctor = Doctor.objects.create(**validated_data)
+        # Extract user data from the flattened fields
+        user_data = validated_data.pop("user")
+        user = CustomUser.objects.create_user(**user_data)
+        doctor = Doctor.objects.create(user=user, **validated_data)
         return doctor
