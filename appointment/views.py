145213570109d1744892911289ee5c008ecd95e1
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-25 00:26:33 -0500

    feat: create ScheduleAppointmentView, AppointmentListView and DeleteAppointmentView

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render  # noqa: F401
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Appointment
+from .serializers import AppointmentSerializer
+
+
+class ScheduleAppointmentView(APIView):
+    def post(self, request):
+        serializer = AppointmentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class AppointmentListView(APIView):
+    def get(self, request):
+        appointments = Appointment.objects.all()
+        serializer = AppointmentSerializer(appointments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DeleteAppointmentView(APIView):
+    def delete(self, request, pk):
+        appointment = Appointment.objects.get(id=pk)
+        appointment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
