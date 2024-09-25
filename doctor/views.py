7d7e5abdfd0a720a5e6ae465bf7c8a625aed9428
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-25 00:29:36 -0500

    feat: create DeleteDoctorView for deleting doctors

--- doctor/views.py
+++ doctor/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Doctor
 from .serializers import DoctorSerializer
+
+User = get_user_model()
 
 
 class RegistrationView(APIView):
@@ -21,3 +24,11 @@ class DoctorListView(APIView):
         doctors = Doctor.objects.all()
         serializer = DoctorSerializer(doctors, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DeleteDoctorView(APIView):
+    def delete(self, request, pk):
+        user = User.objects.get(id=pk)
+        doctor = Doctor.objects.get(user=user)
+        doctor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
