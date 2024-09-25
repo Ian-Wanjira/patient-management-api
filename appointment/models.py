f27438ca4b40730171c4f28a57f35c41672c0897
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-25 00:25:05 -0500

    feat: create appointment model

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,26 @@
-from django.db import models  # noqa: F401
+from django.db import models
 
-# Create your models here.
+from doctor.models import Doctor
+from patient.models import Patient
+
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("scheduled", "Scheduled"),
+        ("cancelled", "Cancelled"),
+    ]
+
+    patient = models.ForeignKey(
+        Patient, on_delete=models.CASCADE, related_name="appointments"
+    )
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, related_name="appointments"
+    )
+    schedule = models.DateTimeField()
+    reason = models.TextField(max_length=500)
+    notes = models.TextField(max_length=500, blank=True, null=True)
+    cancellationReason = models.TextField(max_length=500, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
