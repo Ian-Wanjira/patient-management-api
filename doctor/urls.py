ceca7164ec87810ea416d9d93d47992dbdb4ae3c
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-25 00:28:34 -0500

    feat: add delete endpoint

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import DoctorListView, RegistrationView
+from .views import DeleteDoctorView, DoctorListView, RegistrationView
 
 urlpatterns = [
     path("doctor-registration/", RegistrationView.as_view(), name="register"),
     path("list/", DoctorListView.as_view(), name="list"),
+    path("delete/<pk>/", DeleteDoctorView.as_view(), name="delete"),
 ]
