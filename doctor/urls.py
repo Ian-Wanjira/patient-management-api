aa6fd977615d66680173ecac065d0a2c0a935428
Ian-Wanjira <ianwanjira4@gmail.com>
2024-09-25 23:47:58 -0500

    feat: add doctor detail endpoint

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DeleteDoctorView, DoctorListView, RegistrationView
+from .views import DeleteDoctorView, DoctorDetailView, DoctorListView, RegistrationView
 
 urlpatterns = [
     path("doctor-registration/", RegistrationView.as_view(), name="register"),
     path("list/", DoctorListView.as_view(), name="list"),
     path("delete/<pk>/", DeleteDoctorView.as_view(), name="delete"),
+    path("<pk>/", DoctorDetailView.as_view(), name="detail"),
 ]
