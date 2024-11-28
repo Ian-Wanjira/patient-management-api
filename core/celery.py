147f3119f706d0a94c4b8d88eb154bad8ddd118f
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-28 06:51:34 -0600

    Configure Celery to use Redis with SSL
    - Added  import to manage environment variables.
    - Configured  and  from environment variables.
    - Enabled SSL for broker and backend connections with  set to .

--- core/celery.py
+++ core/celery.py
@@ -1,11 +1,20 @@
 import os
 
+import environ
 from celery import Celery
+
+env = environ.Env()
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 app = Celery("core")
+
+app.conf.broker_url = env("CELERY_BROKER_URL")
+app.conf.result_backend = env("CELERY_RESULT_BACKEND")
+
+app.conf.broker_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
+app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
