9be3385e346f68d3b76f109893110a0543e5771f
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-28 07:17:18 -0600

    refactor: update celery.py to handle ssl issues

--- core/celery.py
+++ core/celery.py
@@ -3,24 +3,27 @@ import os
 import environ
 from celery import Celery
 
+# Initialize environment variables
 env = environ.Env()
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+# Initialize Celery
 app = Celery("core")
 
+# Broker and result backend configuration
 app.conf.broker_url = env("CELERY_BROKER_URL")
 app.conf.result_backend = env("CELERY_RESULT_BACKEND")
 
-app.conf.broker_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
-app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
+# SSL settings for Redis
+if app.conf.broker_url.startswith("rediss://"):
+    app.conf.broker_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
+if app.conf.result_backend.startswith("rediss://"):
+    app.conf.redis_backend_use_ssl = {"ssl_cert_reqs": "CERT_NONE"}
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
+# Load settings from Django settings
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Load task modules from all registered Django apps.
+# Discover tasks from installed Django apps
 app.autodiscover_tasks()
