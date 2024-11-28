8b423e4574547f8576fe1bf687879a6b2ef6f712
Ian-Wanjira <ianwanjira4@gmail.com>
2024-11-28 12:05:43 -0600

    Remove SSL configuration for Redis in Celery
    - Removed explicit SSL configuration from Celery settings.
    - Updated Celery configuration to use plain Redis connections without SSL.
    - This change will disable SSL validation for Redis connections, making the setup simpler for non-production environments.

--- core/celery.py
+++ core/celery.py
@@ -16,14 +16,6 @@ app = Celery("core")
 app.conf.broker_url = env("CELERY_BROKER_URL")
 app.conf.result_backend = env("CELERY_RESULT_BACKEND")
 
-# Explicit SSL configuration for Redis
-app.conf.broker_transport_options = {
-    "ssl": {
-        "ssl_cert_reqs": None  # Use None for no validation or replace with "required" for CERT_REQUIRED
-    }
-}
-app.conf.redis_backend_transport_options = {"ssl": {"ssl_cert_reqs": None}}
-
 # Load settings from Django settings
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
